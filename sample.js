19a2a177e860c8d69f4d6c1a43446d5ddd86beb4
fatemeh <asadij3133@gmail.com>
2023-08-22 10:43:23 +0330

    add removeButton

--- sample.js
+++ sample.js
@@ -1,50 +1,28 @@
 let products = []
 
-// const renderProducts = function (products) {
-//     document.querySelector('#to-do-list').innerHTML = ''
-//     products.forEach(function (item) {
-//         let htmlItem = `<div class="item">
-//         <label for="items${item.id}">
-//             <input type="checkbox" class="items" id="items${item.id}"/>
-//             ${item.title}
-//         </label>
-//         <i class="fal fa-trash-alt remove-icon" id="${item.id}"></i>
-//     </div>`
-//         document.querySelector('#to-do-list').insertAdjacentHTML('beforeend', htmlItem);
-//     })
-// }
-
-
-
-const renderProducts = (product) =>{
+const renderProducts = function (products) {
     document.querySelector('#to-do-list').innerHTML = ''
-        // products.forEach( item => {
-            const parentElement = document.createElement('div')
-            const checkBox = document.createElement('input')
-            const productItem = document.createElement('span')
-            const removeIcon = document.createElement('button')
-        // })
-
-        checkBox.setAttribute('type','checkbox')
-        parentElement.appendChild(checkBox)
-
-        productItem.textContent = product.title
-        parentElement.appendChild(productItem)
-
-        removeIcon.textContent = 'remove'
-        parentElement.appendChild(removeIcon)
-
-        return parentElement
-
+    products.forEach(function (item) {
+        let htmlItem = `<div class="item">
+        <label for="items${item.id}">
+            <input type="checkbox" class="items" id="items${item.id}"/>
+            ${item.title}
+        </label>
+        <i class="fal fa-trash-alt remove-icon" id="${item.id}"></i>
+    </div>`
+        document.querySelector('#to-do-list').insertAdjacentHTML('beforeend', htmlItem);
+    })
 }
 
 
-
-
-
-
-
-
+const removeButton = (id) => {
+    const removeIndex = products.findIndex(item => {
+        return item.id == id
+    })
+    if (removeIndex > -1) {
+        products.splice(removeIndex, 1)
+    }
+}
 
 
 const saveProducts = () => {
@@ -60,22 +38,25 @@ const loadProducts = () => {
     }
 }
 
-const onSubmitClicked = (e) => {
+const SubmitClicked = (e) => {
     e.preventDefault()
     products.push({
         title: e.target.elements.productTitle.value,
-        id: uuidv4(),
-        icon : '<i class="fal fa-trash-alt></i>'
+        id: uuidv4()
     })
     e.target.elements.productTitle.value = ''
     saveProducts()
     renderProducts(products)
 }
 
-document.querySelector('.add-product').addEventListener('submit', onSubmitClicked)
+document.querySelector('.add-product').addEventListener('submit', SubmitClicked)
 loadProducts()
 renderProducts(products)
 
-document.querySelector('#to-do-list').addEventListener('click',function(e){
-    
+
+document.querySelector('#to-do-list').addEventListener('click', function () {
+    removeButton(products.id)
+    saveProducts()
+    renderProducts(products)
+
 })
